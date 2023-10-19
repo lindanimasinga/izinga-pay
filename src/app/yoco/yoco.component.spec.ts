ecb1216d63e1b02baef75d5fbbafe2c1ae422bb9
lindanimasinga <79053807+lindanimasinga@users.noreply.github.com>
2023-10-19 23:29:42 +0200

    ng upgrade

--- src/app/yoco/yoco.component.spec.ts
+++ src/app/yoco/yoco.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { YocoComponent } from './yoco.component';
 
@@ -6,7 +6,7 @@ describe('YocoComponent', () => {
   let component: YocoComponent;
   let fixture: ComponentFixture<YocoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ YocoComponent ]
     })
